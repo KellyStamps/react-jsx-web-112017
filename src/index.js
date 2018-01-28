a060fc03c89272ed5c6441180a3867a4563fc5cb
KellyStamps <kellydsample@gmail.com>
2018-01-28 11:26:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // Code goes here
+
+import {message} from './components/foo';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+//
+//
+// ReactDOM.render (
+//
+//   document.getElementById('global')
+// );
